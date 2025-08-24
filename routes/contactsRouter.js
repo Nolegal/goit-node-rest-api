dc8bcb00e2ba523d5aca1fcf87a1b1bb74f33243
Nolegal <60523956+Nolegal@users.noreply.github.com>
2025-08-24 21:16:11 +0300

    Update contactsRouter.js

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -6,7 +6,7 @@ import {
   createContact,
   updateContact,
 } from "../controllers/contactsControllers.js";
-import { validateBody } from "../helpers/validateBody.js"
+import  validateBody  from "../helpers/validateBody.js"
 import { createContactSchema, updateContactSchema } from "../schemas/contactsSchemas.js";
 
 const contactsRouter = express.Router();
