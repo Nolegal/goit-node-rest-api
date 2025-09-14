c7ed5791de76c59d23954f02ce50cea831d3db77
Nolegal <nolegal@ukr.net>
2025-09-14 14:40:23 +0300

    Avatars-0.1

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -6,14 +6,21 @@ import {
     login,
     logout,
     getCurrent,
+    updateAvatar,
 } from '../controllers/authControllers.js';
 import authenticate from '../middleware/authenticate.js';
-
+import upload from '../middleware/upload.js';
 const authRouter = express.Router();
 
 authRouter.post('/register', validateBody(registerSchema), register);
 authRouter.post('/login', validateBody(loginSchema), login);
 authRouter.post('/logout', authenticate, logout);
 authRouter.get('/current', authenticate, getCurrent);
+authRouter.patch(
+    '/avatars',
+    authenticate,
+    upload.single('avatar'),
+    updateAvatar
+);
 
 export default authRouter;
